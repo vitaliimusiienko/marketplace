0e951a0ab010ceb4a705c784efbbfb5f431f3655
vitaliimusiienko <vitaliimusiienko10@gmail.com>
2024-08-01 15:55:46 +0300

    frontend work fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const App = () => <h1>Hello,React</h1>;
+
+ReactDOM.render(<App />,
   document.getElementById('root')
 );
