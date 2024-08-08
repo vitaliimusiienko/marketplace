15af5933efb41649e49fd69bc179555c90607aa5
Vitalii <160258281+vitaliimusiienko@users.noreply.github.com>
2024-08-08 22:24:42 +0300

    Delete backend/apps/products/permissions.py
    delete permission.py

--- backend/apps/products/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsStaffOrReadOnly(BasePermission):
-
-    def has_permission(self, request, view):
-        if request.method in ['GET', 'HEAD', 'OPTIONS']:
-            return True
-        return request.user and request.user.is_staff
